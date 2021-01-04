25704780866b33411ca6deb3c374efb4219a80af
Kamil <kamil.gorny99@gmail.com>
2021-01-04 19:29:25 +0100

    feat: finish responsive navbar

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fontawesome-free',
     'blog_app.apps.BlogAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
